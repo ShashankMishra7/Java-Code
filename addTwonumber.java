6d042a735260c857b96615d7dcc9d0ad367acde5
Shashank Mishra <60525410+ShashankMishra7@users.noreply.github.com>
2021-06-22 22:59:47 +0530

    Create addTwonumber.java

---
+++ addTwonumber.java
@@ -0,0 +1,14 @@
+ADD TWO NUMBER
+
+public class Add
+{
+    public static void main(String args[])
+    {
+    System.out.println("Add two number");
+    int a=10;
+    int b=20;
+    int sum=a+b;
+   
+    System.out.println(sum);
+    }
+}
