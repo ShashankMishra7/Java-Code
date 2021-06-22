6b4377e6533a798eec6e8c962dc45cc65640fae9
Shashank Mishra <60525410+ShashankMishra7@users.noreply.github.com>
2021-06-23 00:42:04 +0530

    Create addTwoString.java

---
+++ addTwoString.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main
+{
+ public static void main(String args[])
+{
+ Scanner sc= new Scanner(System.in);
+ System.out.println("Enter your nsme ");
+ String str= sc.nextLine();
+ System.out.println("print ");
+ System.out.println("hello "+str+" how are you ");
+}
+}
