7126a7350750f2f396ef6d8e13eab43ae0ed6759
Shashank Mishra <60525410+ShashankMishra7@users.noreply.github.com>
2021-06-22 23:24:03 +0530

    Create userInput.java

---
+++ userInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Input
+{
+ public static void main(String args[])
+{
+Scanner sc= new Scanner(System.in);
+System.out.println("enter string"); 
+String str= sc.nextLine();
+System.out.println("enter string is"); 
+System.out.println(str); 
+}
+}
