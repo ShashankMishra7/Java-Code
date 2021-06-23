fe8f6a323ee26975b81b4081dac8248366085c4a
Shashank Mishra <60525410+ShashankMishra7@users.noreply.github.com>
2021-06-23 15:55:51 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	   String nme="shashank";
+	  String rj= new String("raja"); 
+	  System.out.println(nme);
+	  System.out.println(rj);
+	  
+	  Scanner sc = new Scanner(System.in);
+	  String name= sc.nextLine();
+	  System.out.println(name);
+	}
+}
