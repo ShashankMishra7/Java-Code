4a06bac9957f76e2150c7633c540d37ced5b0acb
Shashank Mishra <60525410+ShashankMishra7@users.noreply.github.com>
2021-06-24 23:24:58 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,19 @@
+public class String
+{
+	public static void main(String[] args) {
+	    String name="harry";
+	    System.out.println(name.length());       //length
+	    int len = name.length();
+	    String nam1= name.toUpperCase();          //toUpperCase/toLowerCase
+	   	System.out.println(name.substring(3));    //substring
+	   	System.out.println(name.charAt(1));      //charAt
+	   	System.out.println(name.indexOf("r"));   //indexOf
+	    String nm=name+"abc"+nam1;            //concat
+	    System.out.println(nm);
+	    System.out.println(name.concat(nam1));
+	    String s1="     Raja    ";
+	    System.out.println(s1.trim());      //trim
+	   	
+	   
+	}
+}
