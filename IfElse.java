9df5cccdb54bd4d165848d82415b86108ded9313
Shashank Mishra <60525410+ShashankMishra7@users.noreply.github.com>
2021-06-25 00:11:25 +0530

    Create IfElse.java

---
+++ IfElse.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class IfElse
+{
+	public static void main(String[] args) {
+	    Scanner sc= new Scanner(System.in);
+	    int a= sc.nextInt();
+	    if(a>18)
+	     		System.out.println("You are adult");
+	    else
+	          	System.out.println("You are not adult");
+	}
+}
