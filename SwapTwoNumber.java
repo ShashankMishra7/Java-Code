15c73829058328598270f175ea8488f9be883baa
Shashank Mishra <60525410+ShashankMishra7@users.noreply.github.com>
2021-06-29 16:30:49 +0530

    Create SwapTwoNumber.java

---
+++ SwapTwoNumber.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int a,b,temp;
+	    a=10;
+	    b=20;
+	    temp=a;
+	    a=b;
+	    b=temp;
+	    System.out.printf("the value of a and b= %d %d",a,b);
+	}
+}       
