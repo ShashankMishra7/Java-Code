8405f0cbd5d6ca1c91ee8b997c5fa05a545d5fc5
Shashank Mishra <60525410+ShashankMishra7@users.noreply.github.com>
2021-07-04 23:40:41 +0530

    Update Arr.java

--- Arr.java
+++ Arr.java
@@ -2,8 +2,11 @@ import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
+	   // int n;
+	   // n=sc.nextInt();
 	    int[] marks;
 	    marks= new int[5];
+	//marks= new int[n];	
 	    Scanner sc= new Scanner(System.in);
 	   for(int i=0; i<marks.length; i++)
 	   {
